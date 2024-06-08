cdb859851f34bdc56bbb4994eab0463dba14b1a8
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-06-08 12:40:24 +0200

    Add Endpoints for user & user profile & register user url.py

--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, views, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from user.serializers import UserSerializer, UserProfileSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class LoginView(TokenObtainPairView):
+    permission_classes = [permissions.AllowAny]
+
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    def get_object(self):
+        return self.request.user
