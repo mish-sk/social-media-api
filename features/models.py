aae27dd8676c83230363dcfc372164f4bce6e7ec
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-06-10 09:25:47 +0200

    Add Post, Comment, Like, Follow models & new migration

--- features/models.py
+++ features/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Follow(models.Model):
+    followers = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="following"
+    )
+    followed = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="followers"
+    )
