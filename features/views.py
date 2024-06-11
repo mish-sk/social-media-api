4430809fb372d125c39be8ae512264e48b51a158
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-06-11 12:27:24 +0200

    Add documentation for drf_spectacular

--- features/views.py
+++ features/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -19,6 +20,20 @@ class PostViewSet(
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="author", description="Filter by author", required=False, type=str
+            ),
+        ]
+    )
+    def list(self, request):
+        """List posts with filter by author"""
+        return super().list(request)
+
 
 class CommentViewSet(
     mixins.ListModelMixin,
@@ -28,6 +43,20 @@ class CommentViewSet(
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="author", description="Filter by author", required=False, type=str
+            ),
+        ]
+    )
+    def list(self, request):
+        """List comments with filter by author"""
+        return super().list(request)
 
 
 class LikeViewSet(
@@ -39,6 +68,20 @@ class LikeViewSet(
     serializer_class = LikeSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="user", description="Filter by user", required=False, type=str
+            ),
+        ]
+    )
+    def list(self, request):
+        """List likes with filter by user"""
+        return super().list(request)
+
 
 class FollowViewSet(
     mixins.ListModelMixin,
@@ -48,3 +91,20 @@ class FollowViewSet(
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(follower=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="follower",
+                description="Filter by follower",
+                required=False,
+                type=str,
+            ),
+        ]
+    )
+    def list(self, request):
+        """List followed with filter by follower"""
+        return super().list(request)
