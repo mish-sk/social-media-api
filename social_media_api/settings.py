6dc9dc0fd191282949abd96e8ad3b0cec596f2a0
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-06-11 18:29:18 +0200

    Add python-dotenv

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tn6ku!n4!u06k6hjjf&wdcmoh*x&-q*47&t1=a@q+h6b0128$)"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
