ad2b7fc369acc19975b1143ee88c03df483c876e
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-06-14 12:03:22 +0200

    Add django-filters & Update requirements.txt

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_spectacular",
+    "django_filters",
     "user",
     "features",
 ]
@@ -54,6 +55,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
 SIMPLE_JWT = {
